f1ff998bacac89b3bf0a19bb4909b3bfd9473609
John <mamudajohn3@gmail.com>
2025-03-13 19:38:54 +0100

    This is my updated Portfolio website

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import ProjectModal from './ProjectModal';
 import './Project.css';
-import MyTeam from './MyTeam';
 
 const ProjectPage = () => {
   const [selectedProject, setSelectedProject] = useState(null);
@@ -12,7 +11,7 @@ const ProjectPage = () => {
       title: 'Grant Univerity',
       description: 'This is the official website of the grant university.',
       image: '/education.jpg', // Leave empty for user to input their own image
-      techStack: ['JSF & Facelets', 'xHTML', 'Java', 'Hibernate','Spring']
+      techStack: ['JSF & Facelets', 'xHTML', 'Java', 'Hibernate', 'Spring']
     },
     {
       id: 2,
@@ -26,13 +25,13 @@ const ProjectPage = () => {
       title: 'ConectProperties',
       description: 'An online real estate website. It shows properties around you that are for sale or for rentage and connects buyers to use.',
       image: '/WhatsApp Image 2024-09-29 at 16.27.45_c150205f.jpg', // Leave empty for user to input their own image
-      techStack: ['React with axios', 'Tailwind css', 'Java', 'Vanilla javascript' ]
-    },{
+      techStack: ['React with axios', 'Tailwind css', 'Java', 'Vanilla javascript']
+    }, {
       id: 4,
       title: 'Lisah Luxury Empire ',
       description: 'An online luxury brand website.',
       image: '/327736374_1647800925676989_6883070318949716767_n.jpg', // Leave empty for user to input their own image
-      techStack: ['React', 'Tailwind css', 'HTML', 'Vanilla javascript' ]
+      techStack: ['React', 'Tailwind css', 'HTML', 'Vanilla javascript']
     },
     // Add more project entries as needed
   ]);
@@ -42,7 +41,7 @@ const ProjectPage = () => {
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.onloadend = () => {
-      const updatedProjects = projects.map(project => 
+      const updatedProjects = projects.map(project =>
         project.id === id ? { ...project, image: reader.result } : project
       );
       setProjects(updatedProjects);
@@ -86,8 +85,6 @@ const ProjectPage = () => {
           onClose={() => setSelectedProject(null)}
         />
       )}
-      
-      <MyTeam/>
     </div>
   );
 };
